3dd01f79f1f8f39cb1e35690c1a03a8d76bb20d6
Josephr1318 <114716973+Josephr1318@users.noreply.github.com>
2024-08-11 14:34:07 -0400

    Add files via upload

---
+++ CapaEntidad/Usuarios.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Usuarios
+    {
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string TipoUsuario { get; set; }
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; } 
+    }
+}
---
+++ CapaEntidad/Visita.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Visita
+    {
+        public int IdVisita { get; set; }
+        public int IdUsuario { get; set; }
+        public int IdAula { get; set; }
+        public string NombreVisitante { get; set; }
+        public string ApellidoVisitante { get; set; }
+        public string Carrera { get; set; }
+        public string Correo { get; set; }
+        public DateTime HoraEntrada { get; set; }
+        public DateTime HoraSalida { get; set; }
+        public string MotivoVisita { get; set; }
+    }
+}
